d300a43d7dee57d1e00102b5c9ee591e5288a89b
Multiplex64 <tycha4630+64@gmail.com>
2025-11-29 03:56:23 +0000

    v0.4.0 error handling overhaul

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import flask
 import os
 import git
+import json
 
 # Flask and GitPython install required! (Through Pip)
+
+
+def replace(inputText, **toInsert):
+    text = inputText
+    for key, value in toInsert.items():
+        text = text.replace("{{" + key + "}}", str(value))
+    return text
 
 
 def get(page: str):
@@ -10,16 +18,25 @@ def get(page: str):
         with open(page, "r") as file:
             return file.read()
     except Exception:
-        return error404()
+        return error(404)
 
 
-def error404():
+def error(e=500, msg=""):
     try:
-        with open("global/404.html", "r") as file:
-            return file.read(), 404
+        with (
+            open("global/http-response.json", "r") as file,
+            open("global/http-response.html", "r") as html,
+        ):
+            data = json.loads(file.read())[str(e)]
+            return (
+                replace(html.read(), error=str(e), errorinfo=data, errormessage=msg),
+                str(e),
+            )
     except Exception:
-        return "Error 404 Not Found (File System Failure)", 404
+        return "500 Internal Server Error", 500
 
+
+print(error(400))
 
 app = flask.Flask(__name__)
 
@@ -35,7 +52,7 @@ def main(path):
                     case "__null":
                         del dir[0]
                         if not dir:
-                            return error404()
+                            return error(404)
                         match dir[0]:
                             case "global":
                                 del dir[0]
@@ -45,7 +62,7 @@ def main(path):
                                             "global", "/".join(dir)
                                         )
                                     else:
-                                        return error404()
+                                        return error(404)
                             case "page":
                                 del dir[0]
                                 if os.path.isfile(
@@ -53,7 +70,7 @@ def main(path):
                                 ):
                                     return get("pages/" + "/".join(dir) + "/index.html")
                                 else:
-                                    return error404()
+                                    return error(404)
                             case "file":
                                 del dir[0]
                                 if os.path.splitext("/".join(dir))[1]:
@@ -62,21 +79,26 @@ def main(path):
                                             "pages", "/".join(dir)
                                         )
                                     else:
-                                        return error404()
+                                        return error(404)
                             case _:
-                                return error404()
+                                return error(404)
                     case _:
-                        pageContent = get("pages/" + "/".join(dir) + "/index.html")
-                        if not isinstance(pageContent, tuple):
-                            pageContent = (pageContent, None)
-
-                        return (
-                            get("global/_index.html").replace(
-                                "!pageContent",
-                                pageContent[0],
-                            ),
-                            pageContent[1],
-                        )
+                        if os.path.splitext("/".join(dir))[1]:
+                            if os.path.isfile("pages/" + "/".join(dir)):
+                                return flask.send_from_directory("pages", "/".join(dir))
+                            else:
+                                return error(404)
+                        else:
+                            pageContent = get("pages/" + "/".join(dir) + "/index.html")
+                            if not isinstance(pageContent, tuple):
+                                pageContent = (pageContent, None)
+                            return (
+                                replace(
+                                    get("global/_index.html"),
+                                    pagecontent=pageContent[0],
+                                ),
+                                pageContent[1],
+                            )
             case "POST":
                 match dir[0]:
                     case "__null":
@@ -88,7 +110,7 @@ def main(path):
                                 repo = git.cmd.Git(
                                     "https://github.com/Multiplex64/Multiplex64/"
                                 )
-                                repo.pull('origin','main')
+                                repo.pull("origin", "main")
                                 return "Updated PythonAnywhere successfully", 200
                             case _:
                                 flask.abort(404)
@@ -97,4 +119,4 @@ def main(path):
             case _:
                 flask.abort(405)
     except Exception:
-        return "Error 500 Internal Server Error (Flask Handler Error)", 500
+        return error(500, "Flask Handler Error")
