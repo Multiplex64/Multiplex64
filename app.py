a7a7f9b303803f46b3ce295de177c79cdeab4f67
Multiplex64 <tycha4630+64@gmail.com>
2025-12-30 16:30:39 -0800

    v18 gitpython 5

--- app.py
+++ app.py
@@ -255,12 +255,12 @@ def update_server() -> tuple[str, int]:
     if event != "push":
         return "Wrong Event type", abort_code
 
-    repo_path = "https://github.com/Multiplex64/Multiplex64/"
-    #git.cmd.Git().pull(repo_path,'main')
+    # repo_path = "https://github.com/Multiplex64/Multiplex64/"
+    # git.cmd.Git().pull(repo_path,'main')
     repo = git.Repo(".")
     origin = repo.remotes.origin
     origin.pull()
-    #repo.remotes.origin.pull()
+    # repo.remotes.origin.pull()
     # repo.git.submodule('update', '--init')
     return "Updated PythonAnywhere successfully", 200
 
